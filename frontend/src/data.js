9673c6dc48860aa90471b1517be83c03000ac66f
Manav Arora <manav10799@gmail.com>
2020-11-02 13:25:16 +0530

    Amazon-Starter

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data={
+    products:[
+        {
+    _id:'1',
+    name:'Nike Shirt',
+    category:'Shirts',
+    image:'/images/p1.jpg',
+    price:1200,
+    brand:'Nike',
+    rating:4.5,
+    numReviews:10,
+    description:'The Nike Sportswear shirt is made from soft cotton fabric for all-day comfort.'
+        },
+        {
+     _id:'2',
+     name:'Adidas Shirt',
+     category:'Shirts',
+     image:'/images/p2.jpg',
+     price:1100,
+     brand:'Adidas',
+     rating:4.0,
+     numReviews:10,
+     description:'The Adidas Sportswear shirt is made from soft cotton fabric for all-day comfort.'
+        },
+        {
+    _id:'3',
+    name:'Zara Shirt',
+    category:'Shirts',
+    image:'/images/p3.jpg',
+    price:1700,
+    brand:'Zara',
+    rating:5.0,
+    numReviews:13,
+    description:'The Zara Shirt is made from soft cotton fabric for all-day comfort.'
+        },
+        {
+    _id:'4',
+    name:'Nike Pants',
+    category:'Pants',
+    image:'/images/p4.jpg',
+    price:2000,
+    brand:'Nike',
+    rating:4.5,
+    numReviews:12,
+    description:'The Nike Sportswear Pants is made from soft cotton fabric for all-day comfort.'
+    },
+    {
+    _id:'5',
+    name:'Puma Pants',
+    category:'Pants',
+    image:'/images/p5.jpg',
+    price:900,
+    brand:'Puma',
+    rating:3.0,
+    numReviews:8,
+    description:'The Puma Sportswear Pants is made from soft cotton fabric for all-day comfort.'
+    },
+    {
+    _id:'6',
+    name:'Adidas Pants',
+    category:'Pants',
+    image:'/images/p6.jpg',
+    price:1500,
+    brand:'Adidas',
+    rating:4.5,
+    numReviews:9,
+    description:'The Adidas Sportswear Pants is made from soft cotton fabric for all-day comfort.'
+    },
+    ],
+}
+
+export default data;
